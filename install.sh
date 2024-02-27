ca91fb15484ac9a83b288720f70eca1e169e7b00
VictorHugoPilled <echase3@gmail.com>
2024-02-27 07:16:05 -0500

    Fixed install.sh for codespaces

--- install.sh
+++ install.sh
@@ -6,18 +6,18 @@ create_symlinks() {
     # get directory containing symlinks
     # use command substitution with pwd so full path is returned
     # type -d: only look for directories
-    # at depth one
     # get files in .zsh folder
     
-    script_dir=$(find $(pwd) -type d -depth 1 -name '.zsh')
+    script_dir=$(find $(pwd) -type d -name '.zsh')
 
     # use find to find all zsh dotfiles
     # . represents the current directory
-    # at depth 2 so we only look in folders in the path
-    # all files with names that start with zsh
+    # set maxdepth 2 so we only look in folders in the path
+    # use path to get all files in the .zsh path
+    # as long as they are not named .DS_Store
     # use xargs to call basename on each filepath so we only get filenames
     
-    for file in $(find . -depth 2 -name '.zsh*' | xargs -n 1 basename);do
+    for file in $(find . -maxdepth 2 -path './.zsh/*' \! -name '.DS_Store' | xargs -n 1 basename);do
 	 echo "Creating symlink to $file in home directory."
 
          # create symlinks for each file in $HOME 
