f4a01b47d0a2a60d03b9434645e95f4970d2c5b1
VictorHugoPilled <echase3@gmail.com>
2024-04-22 10:01:49 -0400

    Added modules

--- mac/pythonstartup.py
+++ mac/pythonstartup.py
@@ -3,3 +3,7 @@ from typing import List, Set, Dict, Tuple
 from collections import *
 from itertools import *
 from functools import *
+from math import *
+from datetime import *
+from pathlib import *
+fro os import *
