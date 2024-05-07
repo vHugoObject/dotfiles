e318359d15ec57b36cfb821b9f8f2933071263ce
VictorHugoPilled <echase3@gmail.com>
2024-05-06 22:06:16 -0400

    Removed and blocked cus-edit from init.el

--- codespaces/.emacs.d/init.el
+++ codespaces/.emacs.d/init.el
@@ -3,15 +3,13 @@
 (tool-bar-mode -1)
 
 (setq warning-minimum-level :emergency)
-
+(setq inhibit-startup-message t)
+(setq dired-kill-when-opening-new-dired-buffer t)
+(setq create-lockfiles nil)
 ;; don't ask for confirmation when opening symlinked file
 (setq vc-follow-symlinks t)     
 
-(setq inhibit-startup-message t)
-(setq dired-kill-when-opening-new-dired-buffer t)
 
-
-(setq create-lockfiles nil)
 
 (defun goto-init-file ()
   "Open the init file."
@@ -26,70 +24,80 @@
   (kill-emacs))
 (global-set-key (kbd "C-x C-c") 'my-kill-emacs)
 
+;; setup use-package
 
-;; path management
 (use-package exec-path-from-shell
   :ensure t)
 ;; necessary to get packages to install on mac
 (when (memq window-system '(mac ns x))
   (exec-path-from-shell-initialize))
-
+(use-package use-package-ensure-system-package :ensure t)
 
 
 ;; package management
-(require 'package)
-(setq package-enable-at-startup nil)
-(package-initialize)
-(setq package-archives '(("org"       . "http://orgmode.org/elpa/")
+(use-package package
+  :custom package-enable-at-startup nil
+	  (package-archives '(("org"       . "http://orgmode.org/elpa/")
                          ("gnu"       . "http://elpa.gnu.org/packages/")
-                         ("melpa"     . "https://melpa.org/packages/")))
+                         ("melpa"     . "https://melpa.org/packages/")))  				     )
+
 
 ;; directory load custom packages from
 ;;(add-to-list 'load-path "~/.emacs.d/custom-packages")
 
-;; packages
-(require 'rust-mode)
-(require 'spray)
+;; only load rust-mode when needed
+(use-package rust-mode
+  :mode "\\.rs\\'"
+)
 
-;; shortcut for spray reader
-(global-set-key (kbd "<f6>") 'spray-mode)
+(use-package js-mode
+  :mode ( "\\.js\\'" "\\.mjs\\'" "\\.jsx\\'")
+)
+
+
+;; bind spray mode f6
+(use-package spray
+  :bind ("C-<f6>" . spray-mode))
 
 ;; org-mode settings
+;; autosave on TODO state chan ge
+(use-package org
+  :hook (org-trigger . save-buffer)
+  :custom
+  (org-todo-keywords
+   '((sequence "TODO(t!)" "WAIT(w@/!)" "|" "DONE(d!)" "CANCELED(c@)")))
+  (org-treat-insert-todo-heading-as-state-change t "log TODO creation")
+  (org-log-into-drawer "LOGBOOK" "log into LOGBOOK drawer")
+  (add-to-list 'org-modules "org-habit" "add habits to org-modules")
+  )
+  
 
-;; add habits to org-modules
-(require 'org)
-(add-to-list 'org-modules "org-habit")
 
-;; define global TODO keywords
-;; ! for a timestamp
-;; @ for a note with a timestamp
-(setq org-todo-keywords
-           '((sequence "TODO(t!)" "WAIT(w@/!)" "|" "DONE(d!)" "CANCELED(c@)")))
+;; org-pomodoro
+(use-package org-pomodoro
+  :ensure t
+  :commands (org-pomodoro)
+  :bind ("M-C-o" . org-pomodoro)
+  ;; autosave on pomodorro finish
+  :hook ((org-pomodoro-finished . save-buffer)
+         (org-pomodoro-started . save-buffer)
+	 (org-pomodoro-break-finished . save-buffer)
+	 )
+  :custom
+   (org-pomodoro-length 20)
+   (org-pomodoro-short-break-length 5)
+   (org-pomodoro-clock-break t)
+   (org-pomodoro-long-break-length 15)
+   (org-pomodoro-manual-break t)
+   )
 
-			
-;; log TODO creation
-(setq org-treat-insert-todo-heading-as-state-change t)
-
-;; log into LOGBOOK drawer
-(setq org-log-into-drawer "LOGBOOK")
-
-;; github codespaces
-(use-package use-package-ensure-system-package :ensure t)
-(use-package codespaces
-  :ensure-system-package gh
-  :config (codespaces-setup)
-  :bind ("C-c S" . #'codespaces-connect))
-    
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(use-package-ensure-system-package spray rust-mode exec-path-from-shell codespaces)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+(use-package magit
+  :ensure t
+  :bind (("C-x C-g" . magit-status)
+        ("C-c p" . magit-push-to-remote)
+	 )
  )
+
+(use-package cus-edit
+  :custom
+  (custom-file null-device "Don't store customizations"))
--- mac/.emacs.d/init.el
+++ mac/.emacs.d/init.el
@@ -98,16 +98,6 @@
 	 )
  )
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(magit org-pomodoro use-package-ensure-system-package spray rust-mode exec-path-from-shell codespaces)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(use-package cus-edit
+  :custom
+  (custom-file null-device "Don't store customizations"))
